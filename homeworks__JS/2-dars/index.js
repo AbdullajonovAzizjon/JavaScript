9c772649688eebf3e075798b75a7a677ec562680
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-11 15:53:47 +0500

    js codes

---
+++ homeworks__JS/2-dars/index.js
@@ -0,0 +1,49 @@
+// let a = 2;
+// let b = 3;     result:over
+
+// let result = (a + b < 4) ? 'below' : 'over';
+
+// console.log(result);
+
+//  part-2
+
+// let a = 8
+
+// switch(a){
+//     case 3:
+//         console.log(`HA bu teng`)
+//         break;
+//         case 7:
+//             console.log(`Ha bu to'gri`)
+//             break;
+//             case 9 :
+//                 console.log(` ha bu to'g'ri`)
+//                 break;
+//                 default:
+//                     console.log(`yo'q bu xato`);
+// }
+
+// part-3
+
+// let id = 9;
+// let id2 = 9;
+// let id3 = `9`;
+
+// id === id2 ? console.log(`Ha bu Teng`) : id2===id3 ? console.log(`Yo'q bu Teng Emas`) :id2==id3 ? console.log(`ha bu Teng`) : console.log(`YO'Q`);
+
+
+// part-4
+
+// let four = 1;
+// let yahoo = 2;
+// let chrome = 3
+
+// let opera = 4
+// if(four > yahoo){
+//     console.log(not);
+// }else if(four > opera){
+//     console.log(not);
+// }else{
+//     console.log(`togri kelmadi`);
+// }
+
