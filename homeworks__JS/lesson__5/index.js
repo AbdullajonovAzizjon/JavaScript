ee7bc9d3ac1fea72cda5ab5959fa37740665e60d
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-12 21:47:43 +0500

    homework

---
+++ homeworks__JS/lesson__5/index.js
@@ -0,0 +1,14 @@
+let age = 20;
+let who = `student`;
+
+
+let chipta = (age < 18 || who===`student`) ? "Chipta narxi 5000 so'm" : "Chipta narxi 10000 so'm";
+console.log(chipta);
+
+
+
+// if(age < 18 || who===`student`){
+//     console.log(`chipta narxi 5000 so'm`);
+// } else{
+//     console.log(`chipta narxi 10000 so'm`);
+// }
