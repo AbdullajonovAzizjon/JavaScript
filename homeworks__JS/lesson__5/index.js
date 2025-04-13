7fcbbafb0d503800316c50ba8d66621c3dd0192d
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-13 23:40:46 +0500

    codes

--- homeworks__JS/lesson__5/index.js
+++ homeworks__JS/lesson__5/index.js
@@ -1,54 +1,8 @@
-// part-1 !!!!!!
-
-// let age = 18 ;
-// let who = `studen`;
-
-
-// let chipta = (age < 18 || who===`student`, prompt(`yoshingizni kiriting`)) ? "Chipta narxi 5000 so'm" : "Chipta narxi 10000 so'm";
-// console.log(chipta);
-
-
-
-// if(age < 18 || who===`student`){
-//     console.log(`chipta narxi 5000 so'm`);
-// } else{
-//     console.log(`chipta narxi 10000 so'm`);
-// }
-
-// let age = 14;
-// let talaba = "ha";
-
-
-//     if (age < Number(18) && talaba === "ha") {
-//         console.log("chipta narxi 5000 so'm");
-
-//     } else {
-//         console.log("chipta narxi 10000so'm.");
-//       }
-//     if(age < Number(18) && talaba === "yo'q"){
-//       console.log("chipta narxi 5000 so'm");
-//     } else{
-//       console.log("chipta narxi 10000so'm.");
-//     }
-//     if(age > Number(18) && talaba === "ha"){
-//       console.log("chipta narxi 5000 so'm");
-//     } else{
-//         console.log("chipta narxi 10000so'm.");
-//     }
-
 let age = Number(prompt("Yoshingizni kiriting:"));
 let talaba = prompt("Talabamisiz? (ha/yo'q)");
 
-if (age < 18 && talaba === "ha") {
-    console.log("chipta narxi 5000 so'm");
-} else if (age < 18 && talaba === "yo'q") {
-    console.log("chipta narxi 5000 so'm");
-} else if (age > 18 && talaba === "ha") {
+if ((age < 18 || talaba === "ha")) {
     console.log("chipta narxi 5000 so'm");
 } else {
     console.log("chipta narxi 10000 so'm");
 }
-
-    
-  
- 
