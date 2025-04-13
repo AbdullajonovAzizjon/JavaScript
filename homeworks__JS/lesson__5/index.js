6bcd33395df3cf104e4af6c40d75b88e189d441b
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-14 00:16:28 +0500

    codes

--- homeworks__JS/lesson__5/index.js
+++ homeworks__JS/lesson__5/index.js
@@ -1,15 +1,44 @@
   // easy-logical__part
 
-  let age = Number(prompt("Yoshingizni kiriting:"));
-  let talaba = prompt("Talabamisiz? (ha/yo'q)");
+//   let age = Number(prompt("Yoshingizni kiriting:"));
+//   let talaba = prompt("Talabamisiz? (ha/yo'q)");
 
-if (age < 18 || talaba === "ha") {
-    console.log("chipta narxi 5000 so'm");
-} else if (age < 18 || talaba === "yo'q") {
-    console.log("chipta narxi 5000 so'm");
-} else if (age > 18 || talaba === "ha") {
-    console.log("chipta narxi 5000 so'm");
+// if (age < 18 || talaba === "ha") {
+//     console.log("chipta narxi 5000 so'm");
+// } else if (age < 18 || talaba === "yo'q") {
+//     console.log("chipta narxi 5000 so'm");
+// } else if (age > 18 || talaba === "ha") {
+//     console.log("chipta narxi 5000 so'm");
+// } else {
+//     console.log("chipta narxi 10000 so'm");
+// }
+
+
+// difficult-logical__part
+
+let mahsulot = prompt("Mahsulot turini kiriting (telefon, laptop, quloqchin):").toLowerCase();
+let soni = Number(prompt("Nechta mahsulot olmoqchisiz?"));
+let chegirma = prompt("Chegirma kartangiz bormi? (ha/yo'q)").toLowerCase();
+
+let narx;
+
+if (mahsulot === "telefon") {
+    narx = 200;
+} else if (mahsulot === "laptop") {
+    narx = 800;
+} else if (mahsulot === "quloqchin") {
+    narx = 50;
 } else {
-    console.log("chipta narxi 10000 so'm");
+    console.log("Noto‘g‘ri mahsulot turi kiritildi.");
+    narx = 0;
 }
+
+let jami = narx * soni;
+
+// Chegirma tekshirish
+if (chegirma === "ha" && jami > 300) {
+    jami = jami * 0.9; // 10% chegirma
+}
+
+console.log("To‘lov summasi: $" + jami);
 
