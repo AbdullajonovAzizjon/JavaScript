9ec03bbcff2bcc5222177d5278a3a21654dc697d
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-14 00:08:26 +0500

    codes

--- homeworks__JS/lesson__5/index.js
+++ homeworks__JS/lesson__5/index.js
@@ -1,14 +1,15 @@
   // easy-logical__part
 
-let age = 20;
-let talaba = `ha`;
+  let age = Number(prompt("Yoshingizni kiriting:"));
+  let talaba = prompt("Talabamisiz? (ha/yo'q)");
 
-if (age < 18 && talaba === "ha") {
+if (age < 18 || talaba === "ha") {
     console.log("chipta narxi 5000 so'm");
-} else if (age < 18 && talaba === "yo'q") {
+} else if (age < 18 || talaba === "yo'q") {
     console.log("chipta narxi 5000 so'm");
-} else if (age > 18 && talaba === "ha") {
+} else if (age > 18 || talaba === "ha") {
     console.log("chipta narxi 5000 so'm");
 } else {
     console.log("chipta narxi 10000 so'm");
 }
+
