29b4f084c1f3125225c46c618c20a06d66702ed8
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-14 15:25:27 +0500

    codes

--- homeworks__JS/lesson__5/index.js
+++ homeworks__JS/lesson__5/index.js
@@ -1,52 +1,30 @@
   // easy-logical__part
 
-
 // let age = prompt("Yoshingizni kiriting:");
-// let talaba = prompt("Talabamisiz? (ha/yo'q)");
+// let student = prompt("Talabamisiz? (ha/yo'q)");
 
-// if (talaba === "ha") {
-//     alert("chipta narxi 5000 so'm");
-// } else if (age < 18) {
-//     alert("chipta narxi 5000 so'm");
-// } else {
-//     alert("chipta narxi 10000 so'm");
-// }
+// let cost = (student === "ha" || age < 18) ? "chipta narxi 5000 so'm" : "chipta narxi 10000 so'm";
 
-let age = prompt("Yoshingizni kiriting:");
-let student = prompt("Talabamisiz? (ha/yo'q)");
-
-let cost = (student === "ha" || age < 18) ? "chipta narxi 5000 so'm" : "chipta narxi 10000 so'm";
-
-alert(cost);
+// alert(cost);
 
 
 
 // difficult-logical__part
 
-// let tool = prompt("Mahsulotni tanlang (telefon, noutbuk, quloqchin)");
-// let soni =prompt("Nechta mahsulot olasiz?");
-// let chegirma = prompt("Chegirma karta bormi? (ha/yo'q)");
+let tool = prompt("Mahsulotni tanlang (telefon, noutbuk, quloqchin)");
+let nechtaligi = prompt("Nechta mahsulot olasiz?");
+let chegirma = prompt("Chegirma karta bormi? (ha/yo'q)");
 
-// let narx;
+let narx = tool === "telefon" ? 200 :
+           tool === "noutbuk" ? 800 :
+           tool === "quloqchin" ? 50 :
+           console.log((alert("Bu mahsulot yo'q"), 0));
 
-// if (tool === "telefon") {
-//     narx = 200;
-// } else if (tool === "noutbuk") {
-//     narx = 800;
-// } else if (tool === "quloqchin") {
-//     narx = 50;
-// } else {
-//     console.log(alert(`Bu mahsulot yo'q`));
-//     narx = 0;
-// }
+let hammasi = narx * nechtaligi;
+hammasi = (chegirma === "ha" && hammasi > 300) ? hammasi * 0.9 : hammasi;
 
-// let hammasi = narx * soni;
+console.log(alert("To'lov summasi: $" + hammasi));
 
-// if (chegirma === "ha" && hammasi > 300) {
-//     hammasi = hammasi * 0.9;
-// }
-
-// console.log(alert("To'lov summasi: $" + hammasi));
 
 
 
