701b0590187367dccc60b57c0205fad1e1b6b593
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-14 17:16:13 +0500

    codes

---
+++ lessons/lesson__6/index.js
@@ -0,0 +1,14 @@
+// for (let i = 1; i <= 10; i++){
+//     for (let j = 1; j <= 10; j++){
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+// for (let i = 0; i < 5; i++){
+//     console.log(i);
+// }
+
+// for ( let i = 0; i < 5; ++i){
+//     console.log(i);
+    
+// }
