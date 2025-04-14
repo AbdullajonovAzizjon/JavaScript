cda78ff4d713985e6044a954ceb6c656c56f898a
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-14 15:10:39 +0500

    codes

--- homeworks__JS/lesson__5/index.js
+++ homeworks__JS/lesson__5/index.js
@@ -1,45 +1,44 @@
   // easy-logical__part
 
-//   let age = (prompt("Yoshingizni kiriting:"));
-//   let talaba = prompt("Talabamisiz? (ha/yo'q)");
+let age = prompt("Yoshingizni kiriting:");
+let talaba = prompt("Talabamisiz? (ha/yo'q)");
 
-// if (age < 18 || talaba === "ha") {
-//     alert("chipta narxi 5000 so'm");
-// } else if (age < 18 || talaba === "yo'q") {
-//     alert("chipta narxi 5000 so'm");
-// } else if (age > 18 || talaba === "ha") {
-//     alert("chipta narxi 5000 so'm");
-// } else if(age > 18 || talaba === "yo'q"){
-//   alert("chipta narxi 10000 so'm");
-// }
+if (talaba === "ha") {
+    alert("chipta narxi 5000 so'm");
+} else if (age < 18) {
+    alert("chipta narxi 5000 so'm");
+} else {
+    alert("chipta narxi 10000 so'm");
+}
+
 
 
 // difficult-logical__part
 
-let tool = prompt("Mahsulotni tanlang (telefon, noutbuk, quloqchin)");
-let soni =prompt("Nechta mahsulot olasiz?");
-let chegirma = prompt("Chegirma karta bormi? (ha/yo'q)");
+// let tool = prompt("Mahsulotni tanlang (telefon, noutbuk, quloqchin)");
+// let soni =prompt("Nechta mahsulot olasiz?");
+// let chegirma = prompt("Chegirma karta bormi? (ha/yo'q)");
 
-let narx;
+// let narx;
 
-if (tool === "telefon") {
-    narx = 200;
-} else if (tool === "noutbuk") {
-    narx = 800;
-} else if (tool === "quloqchin") {
-    narx = 50;
-} else {
-    alert("Bu mahsulot yo'q");
-    narx = 0;
-}
+// if (tool === "telefon") {
+//     narx = 200;
+// } else if (tool === "noutbuk") {
+//     narx = 800;
+// } else if (tool === "quloqchin") {
+//     narx = 50;
+// } else {
+//     console.log(alert(`Bu mahsulot yo'q`));
+//     narx = 0;
+// }
 
-let hammasi = narx * soni;
+// let hammasi = narx * soni;
 
-if (chegirma === "ha" && hammasi > 300) {
-    hammasi = hammasi * 0.9;
-}
+// if (chegirma === "ha" && hammasi > 300) {
+//     hammasi = hammasi * 0.9;
+// }
 
-alert("To'lov summasi: $" + hammasi);
+// console.log(alert("To'lov summasi: $" + hammasi));
 
 
 
