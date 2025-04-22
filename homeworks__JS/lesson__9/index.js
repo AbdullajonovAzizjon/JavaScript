05f5affd16c4acb227a184ef359b7e59339831a5
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-22 22:03:14 +0500

    codes

--- homeworks__JS/lesson__9/index.js
+++ homeworks__JS/lesson__9/index.js
@@ -1,10 +1,21 @@
-let student = {
-    firstName: "Eshmat",
-    lastName: "Toshmatov",
-    age: 20,
-    course: "Frontend"
-  };
-  delete student.course
+// part--1
 
-  console.log(student);
+// let student = {
+//     firstName: "Eshmat",
+//     lastName: "Toshmatov",
+//     age: 20,
+//     course: "Frontend"
+//   };
+//   delete student.course
+
+//   console.log(student);
   
+
+// part--2
+
+let data = {
+    name: "Phone",
+    model: "iPhone",
+    price: 700,
+    amount: 8,
+  };
