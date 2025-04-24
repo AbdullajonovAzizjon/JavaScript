883c5618ce931645e44a46b57900889d57f16728
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-24 22:49:06 +0500

    codes

--- homeworks__JS/lesson__10/index.js
+++ homeworks__JS/lesson__10/index.js
@@ -1,19 +1,56 @@
-let user =  {
-    id : 1,
-    name: "Eshmat",
-    age: 65,
-    status: "Nafaqa oluvchi",
-    child:{
-        id: 2,
-        name: "Toshmat",
-        age:30,
-        status: "Talaba"
-    } 
-}
+// part--1
 
-let cloneUser = structuredClone(user);
+// let user =  {
+//     id : 1,
+//     name: "Eshmat",
+//     age: 65,
+//     status: "Nafaqa oluvchi",
+//     child:{
+//         id: 2,
+//         name: "Toshmat",
+//         age:30,
+//         status: "Talaba"
+//     } 
+// }
+
+// let cloneUser = structuredClone(user);
+// cloneUser.child.age = 45;
+
+// console.log(user);
+// console.log(cloneUser);
+
+
+// part--2
+
+let user =  {
+        id : 1,
+        name: "Eshmat",
+        age: 65,
+        status: "Nafaqa oluvchi",
+        child:{
+            id: 2,
+            name: "Toshmat",
+            age:30,
+            status: "Talaba"
+        } 
+    }
+
+let cloneUser = {};
+
+
+for (key in user){
+    if(typeof user[key] === "object"){
+        cloneUser[key] = {};
+        for (innerkey in user[key]){
+            cloneUser[key][innerkey] = user[key][innerkey]; 
+        }
+    }   else{
+        cloneUser[key] = user[key]
+    }
+}
 cloneUser.child.age = 45;
 
 console.log(user);
 console.log(cloneUser);
+
 
