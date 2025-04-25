4d9a585c8392cb8e8b8fd2064f6d752ed3f6fe31
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-25 07:45:29 +0500

    codes

--- homeworks__JS/lesson__10/index.js
+++ homeworks__JS/lesson__10/index.js
@@ -22,35 +22,52 @@
 
 // part--2
 
+// let user =  {
+//         id : 1,
+//         name: "Eshmat",
+//         age: 65,
+//         status: "Nafaqa oluvchi",
+//         child:{
+//             id: 2,
+//             name: "Toshmat",
+//             age:30,
+//             status: "Talaba"
+//         } 
+//     }
+
+// let cloneUser = {};
+
+
+// for (key in user){
+//     if(typeof user[key] === "object"){
+//         cloneUser[key] = {};
+//         for (innerkey in user[key]){
+//             cloneUser[key][innerkey] = user[key][innerkey]; 
+//         }
+//     }   else{
+//         cloneUser[key] = user[key]
+//     }
+// }
+// cloneUser.child.age = 45;
+
+// console.log(user);
+// console.log(cloneUser);
+
+// part--3
+
 let user =  {
-        id : 1,
-        name: "Eshmat",
-        age: 65,
-        status: "Nafaqa oluvchi",
-        child:{
-            id: 2,
-            name: "Toshmat",
-            age:30,
-            status: "Talaba"
-        } 
-    }
-
-let cloneUser = {};
-
-
-for (key in user){
-    if(typeof user[key] === "object"){
-        cloneUser[key] = {};
-        for (innerkey in user[key]){
-            cloneUser[key][innerkey] = user[key][innerkey]; 
+    id : 1,
+    name: "Eshmat",
+    age: 65,
+    status: "Nafaqa oluvchi",
+    child:{
+        id: 2,
+        name: "Toshmat",
+        age:30,
+        status: "Talaba",
+        func() {
+            return this.age
         }
-    }   else{
-        cloneUser[key] = user[key]
-    }
+    } 
 }
-cloneUser.child.age = 45;
-
-console.log(user);
-console.log(cloneUser);
-
 
