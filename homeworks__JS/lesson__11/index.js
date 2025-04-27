cfa0fcda39504117c333127109102077e9adc0d7
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-27 22:20:31 +0500

    codes

--- homeworks__JS/lesson__11/index.js
+++ homeworks__JS/lesson__11/index.js
@@ -9,6 +9,20 @@ let obj={
 for(key in obj);
 console.log(key);
 
+№--2
+
+let obj = {
+    id: 1,
+    name: "WebBrain"
+};
+
+for ( key in obj) {
+  if (typeof obj[key] === "number") {
+    console.log( obj[key]);
+  }
+}
+
+
 
 
 
