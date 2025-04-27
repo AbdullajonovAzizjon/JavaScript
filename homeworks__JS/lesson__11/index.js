44fee93de61aeea6df5bf082bb028c8869cb4b30
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-04-27 22:24:32 +0500

    codes

--- homeworks__JS/lesson__11/index.js
+++ homeworks__JS/lesson__11/index.js
@@ -1,78 +1,99 @@
-part--1
+// part--1
 
-№--1
-let obj={
-    id:1,
-    name:"WebBrain"
-}
+// №--1
+// let obj={
+//     id:1,
+//     name:"WebBrain"
+// }
 
-for(key in obj);
-console.log(key);
+// for(key in obj);
+// console.log(key);
 
-№--2
+// №--2
 
-let obj = {
+// let obj = {
+//     id: 1,
+//     name: "WebBrain"
+// };
+
+// for ( key in obj) {
+//   if (typeof obj[key] === "number") {
+//     console.log( obj[key]);
+//   }
+// }
+
+
+// №--3
+
+// let obj={
+//     id:1,
+//     name:"WebBrain",
+//     offline:true,
+//     online:true,
+//     individual:false
+// }
+
+// for (key in obj) {
+//   if (typeof obj[key] === "boolean") {
+//     console.log(key + ":" + obj[key]);
+//   }
+// }
+
+
+// №--4
+
+// let obj={
+//     id:1,
+//     name:"WebBrain",
+//     offline:true,
+//     online:true,
+//     individual:false
+// }
+
+// for (key in obj) {
+//     if(typeof obj[key] === "string"){
+//         console.log(key+ ":" + obj[key]
+//         );
+//     }
+// }
+
+
+// №--5
+
+// let obj={
+//   id:1,
+//   name:"atirgul",
+//   color:"qizil",
+//   count:10,
+//   price:"10$"
+// }
+
+// for(key in obj){
+//     if(typeof obj[key] === "string"){
+//         console.log(obj[key]);
+//     }
+// }
+
+
+// №--6
+
+let person = {
     id: 1,
-    name: "WebBrain"
-};
-
-for ( key in obj) {
-  if (typeof obj[key] === "number") {
-    console.log( obj[key]);
-  }
-}
-
-
-№--3
-
-let obj={
-    id:1,
-    name:"WebBrain",
-    offline:true,
-    online:true,
-    individual:false
-}
-
-for (key in obj) {
-  if (typeof obj[key] === "boolean") {
-    console.log(key + ":" + obj[key]);
-  }
-}
-
-
-№--4
-
-let obj={
-    id:1,
-    name:"WebBrain",
-    offline:true,
-    online:true,
-    individual:false
-}
-
-for (key in obj) {
-    if(typeof obj[key] === "string"){
-        console.log(key+ ":" + obj[key]
-        );
+    name: 'Odil',
+    age:78,
+    child: {
+        id: 1,
+        name: 'Ali',
+        age:48,
+        child: {
+            id: 1,
+            name: 'Umar',
+            age:20
+        }
     }
 }
 
-
-№--5
-
-let obj={
-  id:1,
-  name:"atirgul",
-  color:"qizil",
-  count:10,
-  price:"10$"
-}
-
-for(key in obj){
-    if(typeof obj[key] === "string"){
-        console.log(obj[key]);
-    }
-}
-
+let a = person.ageperson.child.age + person.child.child.age;
+console.log(a);
 
 
