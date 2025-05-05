1e485f8c78c5e45bb93c879bfc2f2e25726fe587
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-05-05 14:53:29 +0500

    codes

--- lessons/lesson__14/index.js
+++ lessons/lesson__14/index.js
@@ -29,8 +29,18 @@
 
 // №--3
 
-function truncate(str, n) {
-    return str.slice(0, n);
-  }
+// function truncate(str, n) {
+//     return str.slice(0, n);
+//   }
   
-  console.log(truncate("webbrain academy", 5));  
+//   console.log(truncate("webbrain academy", 5));  
+
+
+// №--4
+
+// function getCurrency(str) {
+//     return Number(str.replace(/\D/g, ""));
+//   }
+  
+//   console.log(getCurrency("$1200")); // 1200
+  
