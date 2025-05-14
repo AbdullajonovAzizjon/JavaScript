182274f7e782260aabdb95111560dbb7212699ce
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-05-14 12:28:07 +0500

    codes

--- homeworks__JS/lesson__17/index.js
+++ homeworks__JS/lesson__17/index.js
@@ -1,58 +1,58 @@
-№--1
-let fruits = ["Apples", "Pear", "Orange"];
+// №--1
+// let fruits = ["Apples", "Pear", "Orange"];
 
-let shoppingcart = fruits;
-shoppingcart.push("Banana");
+// let shoppingcart = fruits;
+// shoppingcart.push("Banana");
 
-console.log(fruits.length); 
+// console.log(fruits.length); 
 
-natija: 4 chunki shoppingcart = fruits deganda u fruitsga teng boladi va shoppingcartda ozgartirilgan ma'lumot  fruitsda ham o'zgaradi.
-
-
----------------------------------------------------------------------------------------------------------------------------------------------------------------
+// natija: 4 chunki shoppingcart = fruits deganda u fruitsga teng boladi va shoppingcartda ozgartirilgan ma'lumot  fruitsda ham o'zgaradi.
 
 
-№--2
-let styles = ["Jazz", "Blues"];
-
-let a = styles;
-a.push("Rock-n-Roll")
-
-console.log(a);
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+// №--2
+// let styles = ["Jazz", "Blues"];
 
-let b = a;
-b.splice(1, 1, "Classics")
-console.log(b);
+// let a = styles;
+// a.push("Rock-n-Roll")
 
- a.shift(0)
-console.log(a);
+// console.log(a);
 
 
 
-b.unshift("Rap", "Reggage");
-console.log(b);
+// let b = a;
+// b.splice(1, 1, "Classics")
+// console.log(b);
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-№---3
-let arr = ["a", "b"];
-
-arr.push(function(){
-    alert( this );
-});
-arr[2]();
-natija: funksiya chiqadi chunki alert( this ); hech nima biriktirmasak u ozini orab turuvchisiga teng boladi. Uni orab turuvchi function. shuning uchun functionga teng. arr.push(function() bu esa arreyga 3 element qoshyapti. oshanda functionning indexi 2ga teng boladi va function chiqadi
+//  a.shift(0)
+// console.log(a);
 
 
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------
+// b.unshift("Rap", "Reggage");
+// console.log(b);
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// №---3
+// let arr = ["a", "b"];
+
+// arr.push(function(){
+//     alert( this );
+// });
+// arr[2]();
+// natija: funksiya chiqadi chunki alert( this ); hech nima biriktirmasak u ozini orab turuvchisiga teng boladi. Uni orab turuvchi function. shuning uchun functionga teng. arr.push(function() bu esa arreyga 3 element qoshyapti. oshanda functionning indexi 2ga teng boladi va function chiqadi
 
 
 
-№---4
+// ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// №---4
 
 
 
