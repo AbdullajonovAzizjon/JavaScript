612d6d3a6cb30de0c168b48d3dd273809322ef22
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-05-21 14:25:56 +0500

    codes

--- homeworks__JS/lesson__18/index.js
+++ homeworks__JS/lesson__18/index.js
@@ -1,24 +1,31 @@
 // №---1
-let students = [
-    {id: 1, name:"Sardor", age: 27},
-    {id: 2, name:"Ismoil", age: 17},
-    {id: 3, name:"Ozod", age: 21},
-];
+// let students = [
+//     {id: 1, name:"Sardor", age: 27},
+//     {id: 2, name:"Ismoil", age: 17},
+//     {id: 3, name:"Ozod", age: 21},
+// ];
   
-const person = students.find(s => s.name === "Sardor");
-console.log(person);
+// const person = students.find(s => s.name === "Sardor");
+// console.log(person);
 
-const aboutage = [...students].sort((a, b) => a.age - b.age);
-console.log(aboutage);
+// const aboutage = [...students].sort((a, b) => a.age - b.age);
+// console.log(aboutage);
 
-const byName = [...students].sort((a, b) => a.name.localeCompare(b.name));
-console.log(byName);
+// const byName = [...students].sort((a, b) => a.name.localeCompare(b.name));
+// console.log(byName);
 
-const byId = [...students].sort((a, b) => a.id - b.id);
-console.log(byId);
+// const byId = [...students].sort((a, b) => a.id - b.id);
+// console.log(byId);
 
 // ----------------------------------------------------------------------------
 
 // №---2
 
+let arr1 = [1, 3, 5];
+let arr2= [2, 4, 6];
 
+let mixed = arr1.concat(arr2);
+console.log(mixed);
+
+let b = mixed.sort((arr1, arr2) => arr1 - arr2);
+console.log(b);
