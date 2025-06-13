30915b8a7a12a490557afc139531cd5e90a74db1
AbdullajonovAzizjon <mrazizjon703@gmail.com>
2025-06-13 14:48:13 +0500

    JS

--- homeworks__JS/lesson__25/index.js
+++ homeworks__JS/lesson__25/index.js
@@ -20,17 +20,17 @@
 
 // countUp();
 
-// №---3
+№---3
 
-// let calc = function factorial(n) {
-//     if (n === 0 || n === 1) {
-//         return 1;
-//     } else {
-//         return n * factorial(n - 1); 
-//     }
-// };
+let calc = function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1); 
+    }
+};
 
-// console.log(calc(5));
+console.log(calc(5));
 
 №---4
 
